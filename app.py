67f204f44b3ea1a2e0b14abf6f840d81b358bf0b
freny1 <166257710+freny1@users.noreply.github.com>
2024-04-13 22:20:19 +0000

    added accounts db to be called in app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from accountsdb import load_from_accounts_db
 
 app = Flask(__name__)
 
 @app.route("/")
-
-def mainmenu():
-  return render_template('home.html')
+def mainmenu(): 
+  accounts = load_from_accounts_db()
+  return render_template('home.html', accounts=accounts)
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
 
